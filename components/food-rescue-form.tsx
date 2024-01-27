03926d79c28160f50d4601a42bd4f41b41a65754
sheng <chengzhisheng2001@gmail.com>
2024-01-27 16:46:32 +0800

    feat: update form disabled state

--- components/food-rescue-form.tsx
+++ components/food-rescue-form.tsx
@@ -100,9 +100,7 @@ export default function FoodRescueForm() {
                     mode='single'
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date('1900-01-01')
-                    }
+                    disabled={(date) => date < new Date()}
                     initialFocus
                   />
                 </PopoverContent>
